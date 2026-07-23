75f9f49209012773fee1e4648c5f7d8b9e6d1f0f
addisoncass <noreenaddison@gmail.com>
2026-07-23 15:21:04 -0500

    updated home page

--- src/app/models/BucketItem.ts
+++ src/app/models/BucketItem.ts
@@ -4,12 +4,14 @@ export class BucketItem {
   description: string
   image: string
   completed: boolean
+  uid?: string
 
-  constructor(idIn: number, titleIn: string, descriptionIn: string, imageIn: string, completedIn: boolean) {
+  constructor(idIn: number, titleIn: string, descriptionIn: string, imageIn: string, completedIn: boolean, uidIn?: string) {
     this.id = idIn
     this.title = titleIn
     this.description = descriptionIn
     this.image = imageIn
     this.completed = completedIn
+    this.uid = uidIn
   }
 }
