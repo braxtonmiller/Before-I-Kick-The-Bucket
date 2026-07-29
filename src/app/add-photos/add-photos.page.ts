24161d3112d0f5323ba48fdb575a6942b17c0fae
Ben Wilson <benwilson@Scotts-MacBook-Pro-4.local>
2026-07-29 11:42:53 -0500

    added functionality for pulling a photo from a device

--- src/app/add-photos/add-photos.page.ts
+++ src/app/add-photos/add-photos.page.ts
@@ -74,11 +74,11 @@ export class AddPhotosPage implements OnInit, OnDestroy {
       const image = await Camera.getPhoto({
         quality: 90,
         allowEditing: false,
-        resultType: CameraResultType.Base64,
+        resultType: CameraResultType.DataUrl,
         source: CameraSource.Prompt
       });
 
-      this.imageBase64 = image.base64String;
+      this.imageBase64 = image.dataUrl;
       console.log("imageBase64: " + this.imageBase64)
 
       if (this.imageBase64) {
--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,6 @@
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import { defineCustomElements } from '@ionic/pwa-elements/loader';
+defineCustomElements(window);
 
 import { AppModule } from './app/app.module';
 
